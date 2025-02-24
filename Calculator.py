9fb1bda8d781c61476b3b42894130de35439a99c
SASYA REDDY <sasyasree1234@gmail.com>
2025-02-24 18:51:39 +0530

    Calculator

---
+++ Calculator.py
@@ -0,0 +1,12 @@
+a = 12
+b = 4
+print(a+b) #Adds a and b
+print(a-b) #Subtracts b from a
+print(a*b) #Multiplies a and b
+print(a/b) #Divides a by b
+print(a**b) #Elevates a to the power of b
+print(a%b)  #The remainder part of the integer division of a by b
+print(a//b) #The integer part of the integer division of a by b
+
+
+
