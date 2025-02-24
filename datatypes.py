e707e15258e7f5d1413d28b84164242ec9762f4e
SASYA REDDY <sasyasree1234@gmail.com>
2025-02-24 19:26:44 +0530

    Datatypes

---
+++ datatypes.py
@@ -0,0 +1,7 @@
+print(type(5)) #integer datatype
+print(type("a")) #string datatype
+print(type(6.97)) #float datatype
+print(8+2)
+print("hello "+"World" )
+print(3+2.5)
+#print(3 + "4") TYPE ERROR- WE CANNOT MIX A TWO INCOMPATIBLE DATATYPES
