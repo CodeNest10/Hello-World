7a733158ab5fc360e9961dab497b1fcd55f1d3a9
SASYA REDDY <sasyasree1234@gmail.com>
2025-02-25 12:42:11 +0530

    Defining Functions

--- func.py
+++ func.py
@@ -1,6 +1,7 @@
-def company_info(name, department):
+def company_info(name,age, department):
     print("Name: "+name)
+    print("Age: "+age)
     print("Department: "+department)
-company_info( "John","Software Development")
-company_info("Raj","Business Analyst")
+company_info( "John",str(27),"Software Development")
+company_info("Raj",str(38),"Business Analyst")
 
